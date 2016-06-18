ce75ca32d09c1815af274a05e5bbf8ace81a7182
IanDCarroll <ian.d.carroll@gmail.com>
2016-06-18 07:34:56 -0700

    initial commit of rps java port

---
+++ rps.java
@@ -0,0 +1,13 @@
+//Java port of rpsRandom (under construction)
+
+/*
+class random {
+    //random function goes here.
+}
+*/
+
+public class rps {
+    public static void main( String [] args ) {
+	System.out.println("24 is the highest number.");
+    }
+}
